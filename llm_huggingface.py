fa74fec8cf0323e801931d711fe60464b1597c09
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-09-02 14:03:53 +0200

    Validate options

--- llm_huggingface.py
+++ llm_huggingface.py
@@ -1,5 +1,6 @@
 import httpx
 import llm
+from pydantic import Field
 
 
 @llm.hookimpl
@@ -11,9 +12,24 @@ class Huggingface(llm.Model):
     model_id = "huggingface"
 
     class Options(llm.Options):
-        max_new_tokens: int = 60
-        temperature: float = 0.2
-        top_p: float = 0.95
+        max_new_tokens: int = Field(
+            default=60,
+            ge=0,
+            le=250,
+            description="Maximum number of tokens to generate.",
+        )
+        temperature: float = Field(
+            default=0.2,
+            gt=0.0,
+            le=100.0,
+            description="Randomness in the response.",
+        )
+        top_p: float = Field(
+            default=0.95,
+            gt=0.0,
+            lt=1.0,
+            description="Nucleus sampling threshold.",
+        )
 
     def execute(self, prompt, stream, response, conversation):
         key = llm.get_key("", "huggingface", "LLM_HUGGINGFACE_KEY")
