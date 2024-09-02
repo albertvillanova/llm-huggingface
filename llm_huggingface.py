d6287bcc75b8ccf33b8e2c9b025c849d46b6a134
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-09-02 12:04:59 +0200

    Implement plugin with HTTP calls to HF Inference API

--- llm_huggingface.py
+++ llm_huggingface.py
@@ -1,3 +1,4 @@
+import httpx
 import llm
 
 
@@ -10,4 +11,23 @@ class Huggingface(llm.Model):
     model_id = "huggingface"
 
     def execute(self, prompt, stream, response, conversation):
-        return ["hello world"]
+        key = llm.get_key("", "huggingface", "LLM_HUGGINGFACE_KEY")
+        headers = {"Authorization": f"Bearer {key}"}
+        url = "https://api-inference.huggingface.co/models/bigcode/starcoder2-15b"
+        body= {
+            "inputs": prompt.prompt,
+            "parameters": {
+                "max_new_tokens": 60,
+                "temperature": 0.2,
+                "top_p": 0.95,
+            },
+        }
+        with httpx.Client() as client:
+            api_response = client.post(
+                url,
+                headers=headers,
+                json=body,
+            )
+            api_response.raise_for_status()
+            yield api_response.json()[0]["generated_text"]
+            response.response_json = api_response.json()
